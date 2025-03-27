eb3696e8e72d9523d680233529f3240ecfe981c9
kanwalkamlesh <159791431+kanwalkamlesh@users.noreply.github.com>
2025-03-27 07:58:09 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
@@ -9,13 +11,11 @@ using namespace std;
 
 // Function to check if placing num at board[row][col] is valid
 bool isValid(int board[N][N], int row, int col, int num) {
-    // Check row and column
     for (int i = 0; i < N; i++) {
         if (board[row][i] == num || board[i][col] == num) 
             return false;
     }
     
-    // Check 3×3 subgrid
     int startRow = row - row % 3, startCol = col - col % 3;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -61,7 +61,7 @@ void printBoard(int board[N][N]) {
         if (i % 3 == 0) cout << "-------------------------\n";
         for (int j = 0; j < N; j++) {
             if (j % 3 == 0) cout << "| ";
-            cout << board[i][j] << " ";
+            cout << (board[i][j] ? to_string(board[i][j]) : " ") << " ";
             if (j == 8) cout << "|";
         }
         cout << endl;
@@ -69,59 +69,55 @@ void printBoard(int board[N][N]) {
     cout << "-------------------------\n";
 }
 
-// Function to fill a 3×3 subgrid randomly
-void fillBox(int board[N][N], int row, int col) {
-    vector<int> numList = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    random_shuffle(numList.begin(), numList.end());
-
-    int index = 0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board[row + i][col + j] = numList[index++];
-        }
-    }
-}
-
-// Function to fill the diagonal 3×3 subgrids randomly
-void fillDiagonal(int board[N][N]) {
-    for (int i = 0; i < N; i += 3) {
-        fillBox(board, i, i);
-    }
-}
-
-// Function to generate a full valid Sudoku board
-void generateSudoku(int board[N][N]) {
-    fillDiagonal(board);
-    solveSudoku(board);
-}
-
-// Function to remove numbers randomly to create a puzzle
-void removeNumbers(int board[N][N], int numToRemove) {
+// Function to randomly fill a few numbers in the Sudoku grid
+void fillRandomNumbers(int board[N][N], int count) {
     srand(time(0));
-    int count = numToRemove;
+    int placed = 0;
 
-    while (count > 0) {
-        int i = rand() % 9;
-        int j = rand() % 9;
-        if (board[i][j] != 0) {
-            board[i][j] = 0;
-            count--;
+    while (placed < count) {
+        int row = rand() % 9;
+        int col = rand() % 9;
+        int num = (rand() % 9) + 1;
+
+        if (board[row][col] == 0 && isValid(board, row, col, num)) {
+            board[row][col] = num;
+            placed++;
         }
     }
+}
+
+// Function to generate a Sudoku puzzle
+void generateSudoku(int board[N][N], int numClues) {
+    fillRandomNumbers(board, numClues);  // Fill with user-defined or random clues
 }
 
 // Main function
 int main() {
-    int sudoku[N][N] = {0};
+    int sudoku[N][N] = {0};  // Start with an empty grid
+    int choice, numClues;
 
-    generateSudoku(sudoku);
-    cout << "Generated Sudoku Puzzle:\n";
+    cout << "Choose an option:\n";
+    cout << "1. Enter the number of clues manually\n";
+    cout << "2. Generate a Sudoku puzzle automatically\n";
+    cout << "Enter your choice (1 or 2): ";
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "Enter the number of clues (min: 17, max: 40): ";
+        cin >> numClues;
+        if (numClues < 17) numClues = 17;
+        if (numClues > 40) numClues = 40;
+    } else {
+        srand(time(0));
+        numClues = rand() % 24 + 17; // Random number of clues between 17 and 40
+    }
+
+    // Generate the Sudoku puzzle
+    generateSudoku(sudoku, numClues);
+    cout << "\nGenerated Sudoku Puzzle:\n";
     printBoard(sudoku);
 
-    removeNumbers(sudoku, 40);
-    cout << "\nSudoku Puzzle after removing numbers:\n";
-    printBoard(sudoku);
-
+    // Solve and print the solution
     cout << "\nSolving Sudoku...\n";
     if (solveSudoku(sudoku)) {
         cout << "Solved Sudoku:\n";
